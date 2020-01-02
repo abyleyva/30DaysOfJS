4e9ead90979590fcabcd6c3af96623e956c8e850
Aby Leyva <abyleyva75@gmail.com>
2020-01-02 16:31:58 -0600

    Details from lesson 1: Day 1 - Introduction & Data Types of JavaScript

--- introduction.js
+++ introduction.js
@@ -1,1 +1,26 @@
+//Declaring Variables Data Types
+let firstName,lastName,country,city;
+let age,isMarried;
+
+//Declaring Const Variables Data Types
+const gravity=9.81
+const bolingPoint=100
+const PI=3.1416
+
+//Declaring variables in one line, separated by comma(,)
+let nickName="Aby Leyva",job='Developer',live='Mexico';
+
+//Set the variables values 
+firstName="Aby";
+lastName='Leyva';
+country='Mexico';
+city='Jalisco';
+age=500;
+isMarried=true;
+
+
+//Show the variables information in console
 console.log('Welcome to 30DaysOfJavaScript External JS File');
+console.log(firstName,lastName,country,city,age,isMarried);
+console.log(gravity,bolingPoint,PI);
+console.log(nickName,job,live);

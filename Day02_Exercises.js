22d45c65685b4bf8a252eb68542ca9c6600555e0
Aby Leyva <abyleyva75@gmail.com>
2020-01-03 23:53:04 -0600

    add changes at Day02 Exercises

--- Day02_Exercises.js
+++ Day02_Exercises.js
@@ -90,3 +90,93 @@ console.log(personInfoGeneral)
 //Boolen result
 console.log(` ${a} is greater then ${b} : ${a > b}`)
 
+//Long listeral strings
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
+console.log(paragraph)
+
+//scape sequences in string
+console.log('I hope every one is enjoying the 30 Days Of JavaScript challenge. \n Do you ?')
+
+console.log('Days \t Topics \ Exercises')
+console.log('Day 1\t3\t5')
+console.log('Day 2\t3\t5')
+console.log('Day 3\t3\t5')
+console.log('Day 4\t3\t5')
+console.log('this is a back slash symbol (\\)')
+console.log('In every programming language it starts with \"Hello, World!\"')
+console.log("In every programming language it starts with \'Hello, World!\'") 
+console.log('The saying \'Seeing is Believing\' is\'t correct in 2020') 
+
+//strings Methods
+
+//length
+console.log(js.length)
+console.log(fullName.length)
+
+//access to different characters in 'Javascript' string
+let string='Javascript'
+let firstLetter=string[0]
+let secondLetter=string[1]
+let thirdLetter=string[2]
+let lastLetter=string[9]
+console.log(firstLetter)
+console.log(lastLetter)
+let lastIndex=string.length-1
+console.log(lastIndex)
+console.log(string[lastIndex])
+
+//ToUppercase
+console.log(string.toUpperCase())
+
+//ToLowerCase
+console.log(string.toLowerCase())
+
+//substr: It takes two arguments,the starting index and number of characters to slice.
+//vasc
+console.log(string.substr(2,4))
+
+//substring(): It takes two arguments,the starting index and the stopping index but it doesn't include the stopping index
+//va
+console.log(string.substring(2,4))
+
+//split(): splits a string at a specified place.
+string='30 Days Of Javascript'
+console.log(string.split())
+console.log(string.split(' '))
+console.log(firstName.split(''))
+
+//trim(): Removes trailing space in the beginning or the end of a string.
+
+string= '   30 Days Of JavaScript   '
+console.log(string)
+console.log(string.trim())
+
+//Include():It checks if a substring exist in a string. returns true if it exists and false if it doesn't exist.
+string = '30 Days Of JavaScript'
+console.log(string.includes('Days'))     // true
+console.log(string.includes('days'))     // false
+console.log(string.includes('Script'))     // true
+console.log(string.includes('script'))     // false
+console.log(string.includes('java'))     // false
+console.log(string.includes('Java'))     // true
+
+//replace(): replace a old substring for new substring
+console.log(string.replace('JavaScript', 'Python'))
+
+//charAt(): Takes index and it returns the value at that index
+console.log(string.charAt(0))
+lastIndex = string.length - 1
+console.log(string.charAt(lastIndex))
+
+//charCodeAt(): Takes index and it returns the value at that index
+console.log(string.charCodeAt(0))
+lastIndex = string.length - 1
+console.log(string.charCodeAt(lastIndex))
+
--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,19 @@
 console.log('Hello, World');
+
+//step 1:
+//Create new source to make git pull like: upstream
+//git remote add upstream https://github.com/Asabeneh/30DaysOfJavaScript.git
+
+//step 2:
+//update our master branch
+//git pull upstream master
+
+//step 3:
+//commit the changes
+//git commit -am "Any Descriptions"
+
+//step 4:
+//send the changes to the our remote repo
+//git push origin master
+
+//and that's it. Enjoy it.
